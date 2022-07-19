ffd5b9029356a5b03355cabfbcf21baa3b109077
BENTLEY\Akshay.Madhukar <akshay.madhukar@bentley.com>
2022-07-19 15:04:55 -0400

    Upgrades PCF dependency

--- cobie-excel-connector/src/dmos/Elements.ts
+++ cobie-excel-connector/src/dmos/Elements.ts
@@ -5,6 +5,7 @@
 import * as pcf from "@itwin/pcf";
 import { GeometryStreamBuilder, GeometryParams } from "@itwin/core-common";
 import { Point3d, LineString3d } from "@itwin/core-geometry";
+import { PConnector } from "@itwin/pcf";
 
 export const FacilityCategory: pcf.ElementDMO = {
   irEntity: "Facility",
@@ -46,7 +47,7 @@ export const Space: pcf.ElementDMO = {
     name: "Space",
     baseClass: "BuildingSpatial:Space",
   },
-  modifyProps(props: any, instance: pcf.IRInstance) {
+  modifyProps(pc: PConnector, props: any, instance: pcf.IRInstance) {
     props.footprintArea = instance.get("GrossArea");
   },
   categoryAttr: "FloorName",
@@ -58,7 +59,7 @@ export const Coordinate: pcf.ElementDMO = {
     name: "COBieCoordinate",
     baseClass: "BuildingSpatial:Space",
   },
-  modifyProps(props: any, instance: pcf.IRInstance) {
+  modifyProps(pc: PConnector, props: any, instance: pcf.IRInstance) {
     const builder = new GeometryStreamBuilder();
 
     // props has category given categoryAttr is defined.
